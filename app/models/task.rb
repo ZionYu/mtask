60c9ffdd89aec9e5dc6bd81edeaece5f80c34b0e
Zion <bone2057@gmail.com>
2018-11-15 01:10:24 +0800

    assign priority with enum

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ApplicationRecord
   validates_presence_of :title
   validate :date_valid?
   enum state: {todo: 0, processing: 1, done: 2 }
+  enum priority: {high: 0, medium: 1, low: 2 }
 
 
   private
