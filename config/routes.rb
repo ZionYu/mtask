21ba40e81d1fe193663df90c2eac6a4df139df0b
Zion <bone2057@gmail.com>
2018-11-16 12:54:52 +0800

    add routes and link for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources "tasks"
+  
   root "tasks#index"
+  resources "tasks"
+  
+  namespace :admin do
+    root "users#index"
+    resources "users"
+  end
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
   get '/login' => 'sessions#new'
