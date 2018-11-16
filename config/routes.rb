794d867c0b153f7a989aa54e19264d00146f4cbf
Zion <bone2057@gmail.com>
2018-11-16 11:57:32 +0800

    setup routes for login/out

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   root "tasks#index"
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 end
