efad86ffe738a151f659c02aaaa798ae58555855
Zion <bone2057@gmail.com>
2018-11-18 02:37:21 +0800

    add rescue_from and exception render error pages

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,11 +1,11 @@
 class Admin::BaseController < ApplicationController
   before_action :authorize
   before_action :authenticate_admin
-
+  
   def authenticate_admin
     unless current_user.admin?
-      redirect_to root_path
-      flash[:alert] = "You are not admin!"  
+      render file: "#{Rails.root}/app/views/errors/not_found", status: :not_found 
     end
   end
+  
 end
