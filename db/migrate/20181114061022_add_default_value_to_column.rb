619082e6aec0454872a54690d762950adeb1c859
Zion <bone2057@gmail.com>
2018-11-18 19:25:05 +0800

    fix undefind method `to_sym'

--- db/migrate/20181114061022_add_default_value_to_column.rb
+++ db/migrate/20181114061022_add_default_value_to_column.rb
@@ -1,5 +1,5 @@
 class AddDefaultValueToColumn < ActiveRecord::Migration[5.2]
   def change
-    change_column :tasks, :state, default: 0, using: 'state::integer'
+    change_column :tasks, :state, :default => 0, :using => 'state::integer'
   end
 end
