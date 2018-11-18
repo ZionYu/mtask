6c4d6beb9aad3e2127905b5e6f3db4ece4670750
Zion <bone2057@gmail.com>
2018-11-18 19:32:34 +0800

    fix pg error in priority column

--- db/migrate/20181114133232_change_priority_of_tasks.rb
+++ db/migrate/20181114133232_change_priority_of_tasks.rb
@@ -1,6 +1,6 @@
 class ChangePriorityOfTasks < ActiveRecord::Migration[5.2]
   def change
-    change_column :tasks, :priority, :integer, default: 0
+    change_column :tasks, :priority, 'integer USING CAST(state AS integer)', default: 0
     add_index :tasks, :priority
   end
 end
