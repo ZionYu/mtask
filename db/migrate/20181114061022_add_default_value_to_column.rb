fbcf16733e5a15115992c8cf46f99eb0908a194e
Zion <bone2057@gmail.com>
2018-11-18 18:54:40 +0800

    modify pg error

--- db/migrate/20181114061022_add_default_value_to_column.rb
+++ db/migrate/20181114061022_add_default_value_to_column.rb
@@ -1,5 +1,5 @@
 class AddDefaultValueToColumn < ActiveRecord::Migration[5.2]
   def change
-    change_column :tasks, :state, :integer, default: 0
+    change_column :tasks, :state, default:0, 'integer USING CAST(state AS integer)'
   end
 end
